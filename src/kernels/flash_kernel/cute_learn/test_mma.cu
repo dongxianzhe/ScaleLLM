87fbfe22572cb2ef56e1c0750a1bbfe70db83c52
Xianzhe Dong <dongxianzhe2019@gmail.com>
2024-07-29 03:59:20 -0400

    simply gemm

--- src/kernels/flash_kernel/cute_learn/test_mma.cu
+++ src/kernels/flash_kernel/cute_learn/test_mma.cu
@@ -3,16 +3,6 @@
 #include<cute/stride.hpp>
 #include<cute/tensor.hpp>
 
-void test(torch::Tensor a, torch::Tensor b, std::string name){
-    if (a.is_cuda())a = a.to(torch::kCPU);
-    if (b.is_cuda())b = b.to(torch::kCPU);
-    float eps = 1e-1;
-    if (a.allclose(b, eps, eps)) {
-        std::cout << name << ": pass" << std::endl;
-    } else {
-        std::cout << name << ": fail" << std::endl;
-    }
-}
 namespace gemm_config {
   using namespace cute;
   constexpr int kTileM = 128;
@@ -20,6 +10,9 @@ namespace gemm_config {
   constexpr int kTileK = 32;
   constexpr int kStage = 3;
   constexpr int kSmemLayoutCBatch = 2;
+  constexpr int kMmaPM = 32;
+  constexpr int kMmaPN = 32;
+  constexpr int kMmaPK = 16;
 
   using SmemLayoutAtom = decltype(composition(
       Swizzle<3, 3, 3>{},
@@ -32,75 +25,50 @@ namespace gemm_config {
       tile_to_shape(SmemLayoutAtom{},
                     make_shape(Int<kTileN>{}, Int<kTileK>{}, Int<kStage>{})));
 
-  constexpr int kMmaPM = 32;constexpr int kMmaPN = 32;constexpr int kMmaPK = 16;
 
   using MMA = decltype(make_tiled_mma(
     SM80_16x8x16_F16F16F16F16_TN{},
     make_layout(make_shape(Int<2>{}, Int<2>{}, Int<1>{})), 
     Tile<Int<kMmaPM>, Int<kMmaPN>, Int<kMmaPK>>{}));
 
-
-  // shared memory to register copy
-  using s2r_copy_op = SM75_U32x4_LDSM_N;
-  using s2r_copy_traits = Copy_Traits<s2r_copy_op>;
-  using s2r_copy_atom = Copy_Atom<s2r_copy_traits, half>;
-
-  using S2RCopyAtomA = s2r_copy_atom;
-  using S2RCopyAtomB = s2r_copy_atom;
-
-  // epilogue: register to global via shared memory
   using SmemLayoutAtomC = decltype(composition(
-      Swizzle<2, 3, 3>{}, make_layout(make_shape(Int<kMmaPM>{}, Int<kMmaPN>{}),
-                                      make_stride(Int<kMmaPN>{}, Int<1>{}))));
+    Swizzle<2, 3, 3>{},
+    make_layout(make_shape(Int<kMmaPM>{}, Int<kMmaPN>{}), make_stride(Int<kMmaPN>{}, Int<1>{}))));
   using SmemLayoutC = decltype(tile_to_shape(
       SmemLayoutAtomC{},
       make_shape(Int<kMmaPM>{}, Int<kMmaPN>{}, Int<kSmemLayoutCBatch>{})));
-
-  static_assert(size<0>(SmemLayoutA{}) * size<1>(SmemLayoutA{}) >= size(SmemLayoutC{}), "C shared memory request is large than A's one pipe");
-
-  using R2SCopyAtomC = Copy_Atom<UniversalCopy<int>, half>;
 }
 
 __global__ void cute_gemm_multi_stage_kernel(void *Dptr, const void *Aptr, const void *Bptr, int m, int n, int k) {
-  using namespace cute;
-  using namespace gemm_config;
-  extern __shared__ half shm_data[];
+  using namespace cute;using namespace gemm_config;
+  int idx = threadIdx.x; int ix = blockIdx.x; int iy = blockIdx.y;
 
-  half *Ashm = shm_data;
-  half *Bshm = shm_data + cute::cosize(SmemLayoutA{});
-
-  int idx = threadIdx.x;int ix = blockIdx.x;int iy = blockIdx.y;
-
-  // use Tensor notation to represent device pointer + dimension
   Tensor A = make_tensor(make_gmem_ptr((half *)Aptr), make_shape(m, k), make_stride(k, Int<1>{}));  // (M, K)
   Tensor B = make_tensor(make_gmem_ptr((half *)Bptr), make_shape(n, k), make_stride(k, Int<1>{}));  // (N, K)
   Tensor D = make_tensor(make_gmem_ptr((half *)Dptr), make_shape(m, n), make_stride(n, Int<1>{}));  // (M, N)
 
-  // slice the tensor to small one which is used for current thread block.
   Tensor gA = local_tile(A, make_tile(Int<kTileM>{}, Int<kTileK>{}), make_coord(iy, _));  // (kTileM, kTileK, k)
   Tensor gB = local_tile(B, make_tile(Int<kTileN>{}, Int<kTileK>{}), make_coord(ix, _));  // (kTileN, kTileK, k)
   Tensor gD = local_tile(D, make_tile(Int<kTileM>{}, Int<kTileN>{}), make_coord(iy, ix));  // (kTileM, kTileN)
 
-  // shared memory
+  extern __shared__ half shm_data[];
+  half *Ashm = shm_data; half *Bshm = shm_data + cute::cosize(SmemLayoutA{});
   auto sA = make_tensor(make_smem_ptr(Ashm), SmemLayoutA{});  // (kTileM, kTileK, kStage)
   auto sB = make_tensor(make_smem_ptr(Bshm), SmemLayoutB{});  // (kTileN, kTileK, kStage)
 
-  // dispatch TileA/TileB/TileC mma tensor into thread fragment via partition
-  // method
   MMA tiled_mma;
   auto thr_mma = tiled_mma.get_slice(idx);
   auto tCrA = thr_mma.partition_fragment_A(gA(_, _, 0));  // (MMA, MMA_M, MMA_K)
   auto tCrB = thr_mma.partition_fragment_B(gB(_, _, 0));  // (MMA, MMA_N, MMA_K)
   auto tCrD = thr_mma.partition_fragment_C(gD);           // (MMA, MMA_M, MMA_N)
-
   clear(tCrD);
 
-  auto s2r_tiled_copy_a = make_tiled_copy_A(S2RCopyAtomA{}, tiled_mma);
+  auto s2r_tiled_copy_a = make_tiled_copy_A(Copy_Atom<SM75_U32x4_LDSM_N, half>{}, tiled_mma);
   auto s2r_thr_copy_a = s2r_tiled_copy_a.get_slice(idx);
   auto tAsA = s2r_thr_copy_a.partition_S(sA);  // (CPY, CPY_M, CPY_K, kStage)
   auto tCrA_view = s2r_thr_copy_a.retile_D(tCrA);  // (CPY, CPY_M, CPY_K)
 
-  auto s2r_tiled_copy_b = make_tiled_copy_B(S2RCopyAtomB{}, tiled_mma);
+  auto s2r_tiled_copy_b = make_tiled_copy_B(Copy_Atom<SM75_U32x4_LDSM_N, half>{}, tiled_mma);
   auto s2r_thr_copy_b = s2r_tiled_copy_b.get_slice(idx);
   auto tBsB = s2r_thr_copy_b.partition_S(sB);  // (CPY, CPY_M, CPY_K, kStage)
   auto tCrB_view = s2r_thr_copy_b.retile_D(tCrB);  // (CPY, CPY_M, CPY_K)
@@ -118,17 +86,12 @@ __global__ void cute_gemm_multi_stage_kernel(void *Dptr, const void *Aptr, const
   int itile_to_read = 0;
   int ismem_read = 0;
   int ismem_write = 0;
-
 #pragma unroll
   for (int istage = 0; istage < kStage - 1; ++istage) {
-    cute::copy(g2s_tiled_copy_ab, tAgA_copy(_, _, _, istage),
-               tAsA_copy(_, _, _, istage));
-    cute::copy(g2s_tiled_copy_ab, tBgB_copy(_, _, _, istage),
-               tBsB_copy(_, _, _, istage));
+    cute::copy(g2s_tiled_copy_ab, tAgA_copy(_, _, _, istage), tAsA_copy(_, _, _, istage));
+    cute::copy(g2s_tiled_copy_ab, tBgB_copy(_, _, _, istage), tBsB_copy(_, _, _, istage));
     cp_async_fence();
-
-    ++itile_to_read;
-    ++ismem_write;
+    ++itile_to_read; ++ismem_write;
   }
 
   cp_async_wait<kStage - 2>();
@@ -162,15 +125,12 @@ __global__ void cute_gemm_multi_stage_kernel(void *Dptr, const void *Aptr, const
 
       if (ik == 0) {
         if (itile_to_read < ntile) {
-          cute::copy(g2s_tiled_copy_ab, tAgA_copy(_, _, _, itile_to_read),
-                     tAsA_copy(_, _, _, ismem_write));
-          cute::copy(g2s_tiled_copy_ab, tBgB_copy(_, _, _, itile_to_read),
-                     tBsB_copy(_, _, _, ismem_write));
+          cute::copy(g2s_tiled_copy_ab, tAgA_copy(_, _, _, itile_to_read), tAsA_copy(_, _, _, ismem_write));
+          cute::copy(g2s_tiled_copy_ab, tBgB_copy(_, _, _, itile_to_read), tBsB_copy(_, _, _, ismem_write));
 
-          ++itile_to_read;
+          ++ itile_to_read;
           ismem_write = (ismem_write + 1) % kStage;
         }
-
         cp_async_fence();
       }
 
@@ -180,7 +140,7 @@ __global__ void cute_gemm_multi_stage_kernel(void *Dptr, const void *Aptr, const
 
   auto sC = make_tensor(sA(_, _, ismem_read).data(), SmemLayoutC{});
 
-  auto r2s_tiled_copy_c = make_tiled_copy_C(R2SCopyAtomC{}, tiled_mma);
+  auto r2s_tiled_copy_c = make_tiled_copy_C(Copy_Atom<UniversalCopy<int>, half>{}, tiled_mma);
   auto r2s_thr_copy_c = r2s_tiled_copy_c.get_slice(idx);
   auto tCrC_r2s = r2s_thr_copy_c.retile_S(tCrD);   // (CPY, CPY_M, CPY_N)
   auto tCsC_r2s = r2s_thr_copy_c.partition_D(sC);  // (CPY, _1, _1, pipe)
@@ -204,7 +164,6 @@ __global__ void cute_gemm_multi_stage_kernel(void *Dptr, const void *Aptr, const
     for (int j = 0; j < step; ++j) {
       auto t = make_tensor_like<half>(tCrC_r2sx(_, i + j));
       cute::copy(tCrC_r2sx(_, i + j), t);
-
       cute::copy(r2s_tiled_copy_c, t, tCsC_r2s(_, 0, 0, j));
     }
     __syncthreads();
@@ -213,7 +172,6 @@ __global__ void cute_gemm_multi_stage_kernel(void *Dptr, const void *Aptr, const
     for (int j = 0; j < step; ++j) {
       cute::copy(s2g_tiled_copy_c, tCsC_s2g(_, 0, 0, j), tCgC_s2gx(_, i + j));
     }
-
     __syncthreads();
   }
 }
@@ -229,16 +187,17 @@ torch::Tensor cute_gemm_multi_stage(torch::Tensor A, torch::Tensor B){
     constexpr int shm_size_AB = cute::cosize(SmemLayoutA{}) + cute::cosize(SmemLayoutB{});
     constexpr int shm_size_C = cute::cosize(SmemLayoutC{});
     constexpr int kShmSize = cute::max(shm_size_AB, shm_size_C) * sizeof(half);
-    int shm_size = kShmSize;
-    cudaFuncSetAttribute(cute_gemm_multi_stage_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, shm_size);
-    cute_gemm_multi_stage_kernel<<<grid, block, shm_size>>>(
-    C.data_ptr(),
-    A.data_ptr(),
-    B.data_ptr(),
-    M, N, K);
+    cute_gemm_multi_stage_kernel<<<grid, block, kShmSize>>>(C.data_ptr(), A.data_ptr(), B.data_ptr(),M, N, K);
     return C;
 }
 
+void test(torch::Tensor a, torch::Tensor b, std::string name){
+    if (a.is_cuda())a = a.to(torch::kCPU);
+    if (b.is_cuda())b = b.to(torch::kCPU);
+    float eps = 1e-1;
+    if (a.allclose(b, eps, eps)) std::cout << name << ": pass" << std::endl;
+    else std::cout << name << ": fail" << std::endl;
+}
 int main(){
     const int M = 1024;const int N = 1024;const int K = 256;
     auto A = torch::randn({M, K}, torch::dtype(torch::kHalf).device(torch::kCUDA));
